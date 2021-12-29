3e408813ecdbd922e1f1e8301b985ad15e94d907
Robert <313prodev@gmail.com>
2021-12-29 21:15:06 +0500

    Delete method for User model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,26 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-     public function index(){
+    public function index()
+    {
         return User::all();
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse|void
+     * DELETE {{base_url}}/api/users/14
+     */
+    public function destroy($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+
+            if ($user->delete()) {
+                return response()->json(['status' => 'success', 'message' => 'User deleted successfully']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
     }
 }
